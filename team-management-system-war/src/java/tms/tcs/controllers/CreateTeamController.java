f744c4bf602cc9d7c2523a793ae7f362f979f509
Maxime Bélair <maxime.belair@sympatico.ca>
2015-12-05 18:35:42 -0500

    fixed bug in createTeam

--- team-management-system-war/src/java/tms/tcs/controllers/CreateTeamController.java
+++ team-management-system-war/src/java/tms/tcs/controllers/CreateTeamController.java
@@ -15,11 +15,14 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.http.HttpSession;
 import org.primefaces.event.TransferEvent;
 import org.primefaces.model.DualListModel;
 import tms.boundaries.CourseFacade;
+import tms.boundaries.StudentFacade;
 import tms.models.Course;
 import tms.models.Student;
+import tms.models.User;
 import tms.tcs.boundaries.TeamFacade;
 import tms.tcs.models.Team;
 
@@ -28,15 +31,18 @@ import tms.tcs.models.Team;
 public class CreateTeamController {
 
     @EJB
+    private StudentFacade studentFacade;
+    @EJB
     private CourseFacade courseFacade;
     @EJB
     private TeamFacade teamFacade;
 
     private String teamName;
-    private DualListModel<Student> studentList;
+    private DualListModel<String> studentList;
     private Team team;
     private Course course;
     private Long courseid;
+    private Student currentStudent;
 
     public CreateTeamController() {
         team = new Team();
@@ -47,27 +53,35 @@ public class CreateTeamController {
             return;
         }
         course = courseFacade.find(courseid);
-        List<Student> studentSource;
-        List<Student> studentTarget;
-        if (studentList == null) {
-            studentSource = new LinkedList<>();
-            studentTarget = new LinkedList<>();
-            boolean toAdd;
-            for (Student s : course.getStudentList()) {
-                toAdd = true;
-                //if s == currentStudent, toAdd = false;   TODO
+
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        Object sessionUser = session.getAttribute("User");
+
+        if (sessionUser != null) {
+            currentStudent = ((User) sessionUser).getStudent();
+        }
+        List<String> studentSource;
+        List<String> studentTarget;
+        studentSource = new LinkedList<>();
+        studentTarget = new LinkedList<>();
+        boolean toAdd;
+        for (Student s : course.getStudentList()) {
+            toAdd = true;
+            if (s.getId().equals(currentStudent.getId())) {
+                toAdd = false;
+            } else {
                 for (Team t : s.getTeamList()) {
                     if (t.getCourse() == course) {
                         //already in a team, don't display in list
                         toAdd = false;
                     }
                 }
-                if (toAdd) {
-                    studentSource.add(s);
-                }
             }
-            studentList = new DualListModel<>(studentSource, studentTarget);
+            if (toAdd) {
+                studentSource.add(s.getId() + " : " + s.getUser().getFirstName() + " " + s.getUser().getLastName());
+            }
         }
+        studentList = new DualListModel<>(studentSource, studentTarget);
     }
 
     public String getTeamName() {
@@ -78,11 +92,11 @@ public class CreateTeamController {
         this.teamName = teamName;
     }
 
-    public DualListModel<Student> getStudentList() {
+    public DualListModel<String> getStudentList() {
         return studentList;
     }
 
-    public void setStudentList(DualListModel<Student> studentList) {
+    public void setStudentList(DualListModel<String> studentList) {
         this.studentList = studentList;
     }
 
@@ -113,10 +127,16 @@ public class CreateTeamController {
     public void submit(ActionEvent actionEvent) {
         team.setName(teamName);
         team.setCourse(course);
-        team.setCreationDate((Timestamp) new Date());
-        //team.setLiaison(currentStudent); //todo
-        List<Student> teamList = studentList.getTarget();
-       // teamList.add(currentStudent); todo
+        team.setCreationDate(new Timestamp(new Date().getTime()));
+        team.setLiaison(currentStudent);
+        List<Student> teamList = new LinkedList<Student>();
+        teamList.add(currentStudent);
+        for (String s : studentList.getTarget()) {
+            Student t = studentFacade.find(s.split(" ")[0]);
+            teamList.add(t);
+            t.getTeamList().add(team);
+            studentFacade.edit(t);
+        }
         team.setStudentList(teamList);
         teamFacade.create(team);
         courseFacade.edit(course);
@@ -133,12 +153,21 @@ public class CreateTeamController {
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
             msg.setSummary("Exceeded number of students in the team");
             FacesContext.getCurrentInstance().addMessage(null, msg);
-            List<Student> studentSource = studentList.getSource();
-            List<Student> studentTarget = studentList.getTarget();
+            List<String> studentSource = studentList.getSource();
+            List<String> studentTarget = studentList.getTarget();
             while (course.getTeamParams().getMaxNumStudents() - 1 < studentList.getTarget().size()) {
                 studentSource.add(studentTarget.remove(studentTarget.size() - 1));
             }
             studentList = new DualListModel<>(studentSource, studentTarget);
         }
     }
+
+    public Student getCurrentStudent() {
+        return currentStudent;
+    }
+
+    public void setCurrentStudent(Student currentStudent) {
+        this.currentStudent = currentStudent;
+    }
+
 }
