c578027e3e101d698271f6b4f3e0e41ebcf5b368
Maxime Bélair <maxime.belair@sympatico.ca>
2015-12-05 20:41:39 -0500

    we can now edit and create teamParameters

--- team-management-system-war/src/java/tms/tcs/controllers/SetupParametersController.java
+++ team-management-system-war/src/java/tms/tcs/controllers/SetupParametersController.java
@@ -18,18 +18,18 @@ import tms.tcs.models.TeamParameters;
 
 /**
  * Controller for the Setup Parameters View
- * 
+ *
  * @author Maxime Bélair, Patrice Boulet
  */
 @ManagedBean(name = "setupParametersController")
 @ViewScoped
 public class SetupParametersController {
-
+    
     @EJB
     private CourseFacade courseFacade;
     @EJB
     private TeamParametersFacade teamParametersFacade;
-
+    
     private TeamParameters teamParameters;
     private Course course;
     private String courseSection;
@@ -37,93 +37,104 @@ public class SetupParametersController {
     private Integer maxStudent;
     private Date deadline;
     private Long courseid;
-
-  
+    
     public SetupParametersController() {
     }
-
+    /*
+     Get the first initialisation of the page, get the course and general information about teams
+     */
+    
     public void init() {
         if (courseid == null) {
             return;
         }
+        //get the team from courseId
         course = courseFacade.find(courseid);
         if (course == null) {
             return;
         }
         courseSection = course.getCourseSection();
-        
+
         // initialize fields with model properties
         if (course.hasTeamParams()) {
-            TeamParameters tp = course.getTeamParams();
-            minStudent = tp.getMinNumStudents();
-            maxStudent = tp.getMaxNumStudents();
-            deadline = tp.getCreationDeadline();
+            teamParameters = course.getTeamParams();
+            minStudent = teamParameters.getMinNumStudents();
+            maxStudent = teamParameters.getMaxNumStudents();
+            deadline = teamParameters.getCreationDeadline();
         }
     }
-
+    
     public Long getCourseid() {
         return courseid;
     }
-
+    
     public void setCourseid(Long courseid) {
         this.courseid = courseid;
     }
-
+    
     public String getCourseSection() {
         return courseSection;
     }
-
+    
     public void setCourseSection(String courseSection) {
         this.courseSection = courseSection;
     }
-
+    
     public Integer getMinStudent() {
         return minStudent;
     }
-
+    
     public void setMinStudent(Integer minStudent) {
         this.minStudent = minStudent;
     }
-
+    
     public Integer getMaxStudent() {
         return maxStudent;
     }
-
+    
     public void setMaxStudent(Integer maxStudent) {
         this.maxStudent = maxStudent;
     }
-
+    
     public Date getDeadline() {
         return deadline;
     }
-
+    
     public void setDeadline(Date deadline) {
         this.deadline = deadline;
     }
-
+    
     public TeamParameters getTeamParameters() {
         return teamParameters;
     }
-
+    
     public void setTeamParameters(TeamParameters teamParameters) {
         this.teamParameters = teamParameters;
     }
-
+    
     public Course getCourse() {
         return course;
     }
-
+    
     public void setCourse(Course course) {
         this.course = course;
     }
-
-    public void submit(ActionEvent actionEvent) {  
-        teamParameters = new TeamParameters();
+    
+    public void submit(ActionEvent actionEvent) {
+        boolean newTeamPara = (teamParameters == null);
+        if (newTeamPara) {
+            teamParameters = new TeamParameters();
+        }
         teamParameters.setCreationDeadline(new Timestamp(deadline.getTime()));
         teamParameters.setMaxNumStudents(maxStudent);
         teamParameters.setMinNumStudents(minStudent);
         course.setTeamParams(teamParameters);
-        teamParametersFacade.create(teamParameters);
+        if (newTeamPara) {
+            teamParametersFacade.create(teamParameters);
+        } else {
+            teamParametersFacade.edit(teamParameters);
+        }
         courseFacade.edit(course);
-    }    
+        
+    }
 }
